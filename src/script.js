8ec825e467184f2207328d5abf6a132807c6e11a
Jeremmie <jeremie@margouillat.net>
2023-06-07 17:21:14 +0200

    update HTML content

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+const container = document.querySelector(".modal_container");
+const slider = document.querySelector(".slider");
+const before = document.querySelector(".img-container-before");
+const after = document.querySelector(".img-container-after");
+
 var modals = document.getElementsByClassName("modal");
 
 for (let index = 0; index < modals.length; index++) {
@@ -29,5 +34,27 @@ for (let index = 0; index < modals.length; index++) {
   }
 
 
+
+
+const dragSlider = (e) => {
+  let x = e.type.includes("mouse") ? e.layerX : e.touches[0].clientX;
+  let size = container.offsetWidth;
+  before.style.width = x + "px";
+  slider.style.left = x + "px";
+  if (x < 30) {
+    before.style.width = 0;
+    slider.style.left = 0;
+  }
+  if (x + 30 > size) {
+    before.style.width = size + "px";
+    slider.style.left = size + "px";
+  }
+};
+
+// Mouse event
+container.addEventListener("mousemove", dragSlider);
+// Touch and drag events
+container.addEventListener("touchstart", dragSlider);
+container.addEventListener("touchmove", dragSlider);
 
 
